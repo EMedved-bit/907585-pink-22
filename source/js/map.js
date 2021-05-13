e731d35ba6882376fc295e3561157233ec368f62
EMedved-bit <eymedvedeva.spb@gmail.com>
2021-05-13 22:15:51 +0300

    Правки по замечаниям

--- source/js/map.js
+++ source/js/map.js
@@ -3,15 +3,15 @@ let myMap;
 ymaps.ready(init);
 
 function init () {
-    myMap = new ymaps.Map('map', {
+    myMap = new ymaps.Map("map", {
         center: [59.931501, 30.314894],
         zoom: 15,
         controls: []
     });
 
     myPlacemark = new ymaps.Placemark([59.9305, 30.3146], {}, {
-      iconLayout: 'default#image',
-      iconImageHref: 'img/icons/map-marker.svg',
+      iconLayout: "default#image",
+      iconImageHref: "img/icons/map-marker.svg",
       iconImageSize: [36, 36],
   })
 
--- source/js/script.js
+++ source/js/script.js
@@ -1,28 +1,28 @@
-const pageHeader = document.querySelector('.page-header');
-const headerNav = document.querySelector('.header-nav');
-const navToggle = document.querySelector('.header-nav__toogle');
-const downloadSection = document.querySelector('.download-section');
-const mainHeader = document.querySelector('.main-header');
+const pageHeader = document.querySelector(".page-header");
+const headerNav = document.querySelector(".header-nav");
+const navToggle = document.querySelector(".header-nav__toogle");
+const downloadSection = document.querySelector(".download-section");
+const mainHeader = document.querySelector(".main-header");
 
-pageHeader.classList.remove('page-header--nojs');
-headerNav.classList.remove('header-nav--nojs');
+pageHeader.classList.remove("page-header--nojs");
+headerNav.classList.remove("header-nav--nojs");
 
 if(downloadSection) {
-  downloadSection.classList.remove('download-section--nojs');
+  downloadSection.classList.remove("download-section--nojs");
 }
 
 if(mainHeader) {
-  mainHeader.classList.remove('main-header--nojs');
+  mainHeader.classList.remove("main-header--nojs");
 }
 
-navToggle.addEventListener('click', function() {
-  if(headerNav.classList.contains('header-nav--closed')) {
-    headerNav.classList.remove('header-nav--closed');
-    headerNav.classList.add('header-nav--opened');
-    pageHeader.classList.add('page-header--menu-opened');
+navToggle.addEventListener("click", function() {
+  if(headerNav.classList.contains("header-nav--closed")) {
+    headerNav.classList.remove("header-nav--closed");
+    headerNav.classList.add("header-nav--opened");
+    pageHeader.classList.add("page-header--menu-opened");
   } else {
-    headerNav.classList.add('header-nav--closed');
-    headerNav.classList.remove('header-nav--opened');
-    pageHeader.classList.remove('page-header--menu-opened');
+    headerNav.classList.add("header-nav--closed");
+    headerNav.classList.remove("header-nav--opened");
+    pageHeader.classList.remove("page-header--menu-opened");
   }
 })
